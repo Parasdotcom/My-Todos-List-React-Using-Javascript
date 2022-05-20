72348fd5b3a796c5f20298c7213f4ee49ddb0bff
Paras Saxena <85274192+Parasdotcom@users.noreply.github.com>
2022-05-20 19:21:27 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>This is my React Application Using a Javascipt Created by Paras Saxena</p>
+    </div>
+  )
+}
+
+export default About;
